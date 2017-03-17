aaef8a9941fd2be9798f094fdcd7801a963b5a7c
schvabodka-man <scvhapps@gmail.com>
2017-03-17 20:25:24 +0200

    Initial commit

---
+++ app/src/main/java/apps/scvh/com/whattodo/activity/Click.java
@@ -0,0 +1,27 @@
+package apps.scvh.com.whattodo.activity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import apps.scvh.com.whattodo.R;
+import fragments.Movie;
+
+public class Click extends FragmentActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.push_button) {
+            Intent showMovie = new Intent(this, MovieRolled.class);
+            startActivity(showMovie);
+        }
+    }
+}
---
+++ app/src/main/java/apps/scvh/com/whattodo/activity/MovieRolled.java
@@ -0,0 +1,15 @@
+package apps.scvh.com.whattodo.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import apps.scvh.com.whattodo.R;
+
+public class MovieRolled extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_rolled);
+    }
+}
