e59afad3789e9241b549a9c08ec025f1de857762
schvabodka-man <scvhapps@gmail.com>
2017-04-06 21:33:58 +0300

    RxJava click listening

--- app/src/main/java/apps/scvh/com/whattodo/ui/activity/Click.java
+++ app/src/main/java/apps/scvh/com/whattodo/ui/activity/Click.java
@@ -3,24 +3,21 @@ package apps.scvh.com.whattodo.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
+
+import com.jakewharton.rxbinding2.view.RxView;
 
 import apps.scvh.com.whattodo.R;
 
-public class Click extends FragmentActivity implements View.OnClickListener {
+public class Click extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click);
-        findViewById(R.id.push_button).setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.push_button) {
+        RxView.clicks(findViewById(R.id.push_button)).subscribe(t -> {
             Intent showMovie = new Intent(this, MovieRolled.class);
             startActivity(showMovie);
-        }
+        });
     }
+
 }
