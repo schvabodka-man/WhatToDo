27039d545641c33e5103ca53d87438902aad0fec
schvabodka-man <scvhapps@gmail.com>
2017-04-15 17:27:25 +0300

    Check if there is network connection

--- app/src/main/java/apps/scvh/com/whattodo/ui/activity/Click.java
+++ app/src/main/java/apps/scvh/com/whattodo/ui/activity/Click.java
@@ -1,8 +1,12 @@
 package apps.scvh.com.whattodo.ui.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.jakewharton.rxbinding2.view.RxView;
 
@@ -15,8 +19,17 @@ public class Click extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click);
         RxView.clicks(findViewById(R.id.push_button)).subscribe(t -> {
-            Intent showMovie = new Intent(this, MovieRolled.class);
-            startActivity(showMovie);
+            ConnectivityManager cm =
+                    (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            boolean isInternet = activeNetwork != null &&
+                    activeNetwork.isConnectedOrConnecting();
+            if (isInternet) {
+                Intent showMovie = new Intent(this, MovieRolled.class);
+                startActivity(showMovie);
+            } else {
+                Toast.makeText(this, R.string.no_internet, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
